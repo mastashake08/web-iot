00111f3b26e01529127fa86c1b0f15b59e510972
Hacker God <jyrone.parker@gmail.com>
2023-10-23 12:41:14 -0400

    v1.3.0

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { WebIOT } from './classes/WebIOT'
 import { NFCManager } from './classes/NFCManager'
 import { SerialManager } from './classes/SerialManager'
-import { SerialManager } from './classes/SerialManager'
+import { BluetoothManager } from './classes/BluetoothManager'
 import { USBManager } from './classes/USBManager'
 export {
   WebIOT,
   NFCManager,
   SerialManager,
-  SerialManager,
+  BluetoothManager,
   USBManager
 }
